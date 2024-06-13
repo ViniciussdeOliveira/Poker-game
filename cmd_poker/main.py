7bec3c39411b23e8777c3cd7593b5591a1ecc55c
Vinicius de Oliveira Santos <githubdovinicius@gmail.com>
2024-06-13 12:04:21 -0300

    Fix: Resolução de problemas encontrados

--- cmd_poker/main.py
+++ cmd_poker/main.py
@@ -13,13 +13,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-""" TODO:
-x Criar a funcao resetar mao em jogadores
-x melhorar o reset do jogo em jogar-POKER()
-x limpar o código
-x separar as classes
-x incluir a licença MIT
-- fazer o readme
-- mudar o qtd_nomes_jogadores para armazenar as posições em vez de quantos são
-- começar a classe que analisa as maos para gerar o que ele tem
- """
